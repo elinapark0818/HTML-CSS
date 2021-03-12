8a76052acbff09a63547afd9cbc52eb5f4fd3569
ElinaPark <elinapark0818@gmail.com>
2021-03-12 22:16:00 +0900

    하...모달 내일 다시보자

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.querySelector('.modal-wrapper');
+
+open.onclick = () => {
+    modal.style.display = 'flex';
+};
+close.onclick = () => {
+    modal.style.display = 'none';
+};
